928c47deecd46ec09c2e19dd705f0d5982ec97e6
bkenzhaliyev <bkenzhaliyev@gmail.com>
2021-06-30 16:53:37 +0600

    Initial commit

---
+++ src/hw5/Person.java
@@ -0,0 +1,11 @@
+package hw5;
+
+public class Person {
+    private String name;
+    private String position;
+    private String mail;
+    private String phoneNum;
+    private double salary;
+    private int age;
+
+}
---
+++ src/hw5/hw5.java
@@ -0,0 +1,9 @@
+package hw5;
+
+public class hw5 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
