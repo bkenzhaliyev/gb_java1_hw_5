4c2e0abe2edea71558671ae698351bc2cae65808
bkenzhaliyev <bkenzhaliyev@gmail.com>
2021-06-30 16:56:34 +0600

    hw5

--- src/hw5/Person.java
+++ src/hw5/Person.java
@@ -8,4 +8,20 @@ public class Person {
     private double salary;
     private int age;
 
+    public Person(String name, String position, String mail, String phoneNum, double salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.mail = mail;
+        this.phoneNum = phoneNum;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info(){
+        System.out.printf("%-25s %-25s %-25s %-20s %10s %10s \n", name, position, mail, phoneNum, salary, age);
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
