333fa44ff8d1016be97b9e32b943e3389455fc2c
bkenzhaliyev <85982897+bkenzhaliyev@users.noreply.github.com>
2021-07-04 23:32:23 +0600

    Revert "Hw6"

--- src/hw5/Person.java
+++ src/hw5/Person.java
@@ -8,30 +8,4 @@ public class Person {
     private double salary;
     private int age;
 
-    public Person(String name, String position, String mail, String phoneNum, double salary, int age) {
-        this.name = name;
-        this.position = position;
-        this.mail = mail;
-        this.phoneNum = phoneNum;
-        this.salary = salary;
-        this.age = age;
-    }
-
-    public void info(int num){
-        System.out.printf("%3s  %-25s %-25s %-25s %-20s %10s %10s \n", num, name, position, mail, phoneNum, salary, age);
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
 }
-
